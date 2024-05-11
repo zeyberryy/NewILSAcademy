7d62a00173922f88a77b3259f6bfd9c4a742f461
Albin2248 <albinjose2248@gmail.com>
2024-05-11 09:16:56 +0530

    update head.php

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link href="img/logo.png" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
